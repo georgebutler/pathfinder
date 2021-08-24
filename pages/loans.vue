1c71b7230e9018bcf29f9ee56e45d7b24e8ab222
George Butler <georgebutleriv@gmail.com>
2021-08-24 16:57:36 -0700

    Button component

--- pages/loans.vue
+++ pages/loans.vue
@@ -5,8 +5,18 @@
     <div class="text-lg font-bold">
       Loans
     </div>
-    <div v-for="loan in loans" :key="loan.id">
-      {{ loan.type }}
+    <div v-if="loading">
+      Loading
+    </div>
+    <div v-else>
+      <div v-for="loan in loans" :key="loan.id" class="bg-gray-100">
+        <div>{{ loan.type }}</div>
+        <div>{{ loan.amount }}</div>
+        <div>Rate: {{ loan.rate }}</div>
+        <div>Term: {{ loan.termInDays }} Days</div>
+        <div>{{ loan.collateralRequired ? 'Collateral Required' : 'No Collateral Required' }}</div>
+        <Button>Aquire Loan</Button>
+      </div>
     </div>
   </div>
 </template>
@@ -17,7 +27,8 @@ export default {
   data () {
     return {
       errors: [],
-      loans: []
+      loans: [],
+      loading: true
     }
   },
   computed: {
@@ -34,6 +45,9 @@ export default {
         this.errors.push({
           message: 'networkError'
         })
+      })
+      .finally(() => {
+        this.loading = false
       })
   }
 }
