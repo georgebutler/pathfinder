165f47841769625a1c96cbf527aebd5046459b64
George Butler <georgebutleriv@gmail.com>
2021-08-24 18:41:59 -0700

    Alert Icon

--- components/Alert.vue
+++ components/Alert.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="flex items-center bg-red-500 text-white text-sm font-bold px-4 py-3">
-    <p>{{ message }}</p>
+  <div class="rounded-md bg-red-50 p-4">
+    <div class="flex">
+      <div class="flex-shrink-0">
+        <ExclamationIcon class="text-red-700" />
+      </div>
+      <div class="ml-3 flex-1 md:flex md:justify-between">
+        <div class="text-sm text-red-700">
+          {{ message }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
